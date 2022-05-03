4acb91f7db05fc52bfc7b8b04bd3c06dd97877dc
elhedgie <98222227+elhedgie@users.noreply.github.com>
2022-05-03 15:16:55 +0300

    codestyle fixes

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,11 +1,12 @@
 let defaultState = []
 
+
 export default function usersReducer(state = defaultState, action) {
   switch (action.type) {
     case "FETCH":
-      return (defaultState = [
-        ...defaultState, ...action.payload,
-      ])
+      return defaultState = [
+        ...state, ...action.payload
+      ]
     default:
       return state;
   }
